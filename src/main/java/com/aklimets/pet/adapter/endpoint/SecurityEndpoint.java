3bf94126f9d7e11df24729d4d4cdf181ec7aa7d0
aliaksandrklimets <aliaksandrklimets@gmail.com>
2024-09-12 20:28:20 +0200

    Add confirmation endpoint

--- src/main/java/com/aklimets/pet/adapter/endpoint/SecurityEndpoint.java
+++ src/main/java/com/aklimets/pet/adapter/endpoint/SecurityEndpoint.java
@@ -8,10 +8,7 @@ import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/unauthenticated/security/v1")
@@ -34,5 +31,10 @@ public class SecurityEndpoint {
     @PostMapping("/register")
     public ResponseEntity<?> register(@Valid @RequestBody RegistrationRequest request) {
         return securityService.register(request);
+    }
+
+    @GetMapping("/profile/confirm/{code}")
+    public ResponseEntity<?> confirmProfile(@PathVariable String code) {
+        return securityService.confirmProfile(code);
     }
 }
--- src/main/java/com/aklimets/pet/application/service/SecurityService.java
+++ src/main/java/com/aklimets/pet/application/service/SecurityService.java
@@ -5,7 +5,6 @@ import com.aklimets.pet.domain.dto.request.JwtRefreshTokenRequest;
 import com.aklimets.pet.domain.dto.request.RegistrationRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +14,7 @@ public class SecurityService {
     private static final String AUTHENTICATION_ENDPOINT_URL_V1 = "/api/v1/security/authenticate";
     private static final String REFRESH_ENDPOINT_URL_V1 = "/api/v1/security/refresh";
     private static final String REGISTRATION_ENDPOINT_URL_V1 = "/api/v1/security/register";
+    private static final String CONFIRM_PROFILE_ENDPOINT_URL_V1 = "/api/v1/security/profile/confirm/";
 
     @Value("${security.app.url}")
     public String securityEndpoint;
@@ -32,5 +32,9 @@ public class SecurityService {
 
     public ResponseEntity<?> register(RegistrationRequest request) {
         return restService.exchangePostWrapped(securityEndpoint + REGISTRATION_ENDPOINT_URL_V1, request);
+    }
+
+    public ResponseEntity<?> confirmProfile(String code) {
+        return restService.exchangeGetWrapped(securityEndpoint + CONFIRM_PROFILE_ENDPOINT_URL_V1 + code);
     }
 }
